76d48148bf6aff740d77d7d0a88bf979ea2fd49b
achintb2 <74408248+achintb2@users.noreply.github.com>
2021-01-15 14:58:12 +0530

    Added fixed color option alongside hue cycling

--- AudioVisualizer-Arduino-Windows.ino
+++ AudioVisualizer-Arduino-Windows.ino
@@ -1,9 +1,9 @@
-//getswitchstate update
+//finalizing...
 #include <FastLED.h>
 #define LED_PIN     3
 #define NUM_LEDS    300
 #define BUTTON_PIN  8
-
+#define onboardLED  13
 CRGB leds[NUM_LEDS];
 int sat = 255; int light = 255;
 String inString = "";
@@ -13,7 +13,8 @@ void setup()
     FastLED.addLeds<WS2812B, LED_PIN, GRB>(leds, NUM_LEDS);
     fill_solid(leds, NUM_LEDS, CRGB(0,0,0));
     Serial.begin(128000);
-    pinMode(BUTTON_PIN, INPUT_PULLUP);
+    pinMode(8, INPUT_PULLUP);
+    pinMode(onboardLED, OUTPUT);
 }
 
 bool toggState;
@@ -26,41 +27,65 @@ bool getSwitchState()
     if(mySwitch == HIGH)
     {
         currState = !currState;
+        digitalWrite(onboardLED, HIGH);
+        delay(500);
+        digitalWrite(onboardLED, LOW);
     }
     return(currState);
 }
 
-void loop()
+int getValue()
 {
-    for(int h=0; h<256; h++)
+    int color = 0;
+    int inChar = 0;
+    int wait = 0;
+    while(inChar!= '\n' and wait < 25000)
     {
-        int inChar = 0;
-        int wait = 0;
-        
-        while(inChar!= '\n' and wait < 10000)
+        inChar = 0;
+        if(Serial.available() > 0)
         {
-            inChar = 0; sat = 255;
-            if(Serial.available() > 0)
+            inChar = Serial.read();
+            inString += (char)inChar;
+            if(inChar == '\n')
             {
-                inChar = Serial.read();
-                inString += (char)inChar;
-                if(inChar == '\n')
-                {
-                    light = inString.toInt();
-                    sat -= 2*(light-220);
-                    sat = max(0, min(sat, 255));
-                    light = max(10, min(light, 255));
-                    inString = "";
-                }
-            }
-            else{light=255;}
-            wait++;
-            if(getSwitchState() == HIGH)
-            {
-                light = 0;
+                color = inString.toInt();
+                inString = "";
             }
         }
-        fill_solid(leds, 150, CHSV(h,sat,light));
-        FastLED.show();
+        else{color=1;}
+        wait++;
     }
+    return(color);
+}
+
+void HSVMode(int xtraHue)
+{
+    int hue = 0, sat = 0, lit = 0;
+    int altHue = 0;
+    hue = getValue(); sat = getValue(); lit= getValue();
+    altHue = hue;
+    altHue += xtraHue;
+
+    //Change this color to altHue for a color cycling effect, otherwise fixed blue color right now.
+    if(hue == 1 && sat == 1 && lit == 1)
+    {hue = 151; sat = 255; lit = 255;}
+    else
+    {hue = altHue;}
+
+    if(getSwitchState() == LOW)
+    {
+        lit = 0;
+    }
+    
+    hue = max(0, hue); 
+    lit = max(0, lit); 
+    sat = max(0, sat);
+    fill_solid(leds, 250, CHSV(hue,sat,lit));
+    FastLED.show();
+}
+
+void loop()
+{
+    for(int xh = 0; xh < 255; xh++)
+    {HSVMode(xh);}
 }
